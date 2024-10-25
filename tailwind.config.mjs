696961ce5135f549efc47e3b22c307c9f250be99
Helena Pfeffer <hepf0001@stud.kea.dk>
2024-10-25 11:21:27 +0200

    Fonte + cusor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                lato: ["Lato", "sans-serif"],
+                kanit: ["Kanit", "sans-serif"],
+              },
+            cursor: {
+                'handcursor': "url(/cursor.png) 100 100, pointer",
+              },
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
